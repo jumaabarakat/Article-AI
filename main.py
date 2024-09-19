fc9bdd0635247781c732c09769b4da766433bd2d
jumaabarakat <mohamadjumaa718@gmail.com>
2024-09-19 16:43:01 +0400

    push

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from article_functions import detect_language, extract_pdf_text, generate_article_from_content, save_article_to_docx
 from flask import Flask, request, jsonify, send_file
 import os
-
+from youtube_generator import extract_video_id, get_video_transcript, generate_summary
+from article_functions import extract_pdf_text, detect_language, generate_article_from_content, save_article_to_docx
 
 
 app = Flask(__name__)
@@ -43,6 +43,39 @@ def generate_article():
     except Exception as e:
         # Return error message if something goes wrong
         return jsonify({'error': str(e)}), 400
+
+@app.route('/generate-summary', methods=['POST'])
+def generate_summary_api():
+    try:
+        data = request.json
+        url = data.get('youtube_url')
+        language = data.get('language', 'English')
+        type_of_summary = data.get('type_of_summary', 'Short')
+
+        # Validate inputs
+        if not url or language not in ['Arabic', 'English'] or type_of_summary not in ['Detailed', 'Short']:
+            return jsonify({"error": "Invalid input"}), 400
+        
+        # Extract video ID from the URL
+        video_id = extract_video_id(url)
+        if not video_id:
+            return jsonify({"error": "Invalid YouTube URL"}), 400
+        
+        # Get video transcript
+        transcript_text = get_video_transcript(video_id,language)
+        
+        # Generate summary based on the transcript
+        summary = generate_summary(transcript_text, language, type_of_summary)
+        
+        # Return the summary as a JSON response
+        return jsonify({
+            "summary": summary,
+            "language": language,
+            "type_of_summary": type_of_summary
+        }), 200
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
